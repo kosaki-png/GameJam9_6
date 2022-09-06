ce1cf6377d4c27fc4c9950c3ea12b23ac2829047
newshell <52154054+newshell3125@users.noreply.github.com>
2022-09-06 16:22:09 +0900

    taget基底クラス追加

---
+++ DirectXGame/BaseTaget.cpp
@@ -0,0 +1,18 @@
+#include "BaseTaget.h"
+
+BaseTaget::~BaseTaget()
+{
+}
+
+void BaseTaget::Initialize(std::string modelName)
+{
+}
+
+void BaseTaget::Update()
+{
+}
+
+void BaseTaget::Draw()
+{
+	
+}
---
+++ DirectXGame/BaseTaget.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "base/Vector3.h"
+#include "3d/Object3d.h"
+#include "3d/Model.h"
+#include "base/SafeDelete.h"
+
+class BaseTaget
+{
+private:
+	BaseTaget();
+	~BaseTaget();
+
+	//èâä˙âª
+	virtual void Initialize(std::string modelName);
+	//çXêV
+	virtual void Update();
+	//ï`âÊ
+	virtual void Draw();
+
+	//Set
+	void SetPosition(Vector3 pos) { position = pos; }
+
+	//Get
+	Vector3 GetPosition() { return position; }
+	Vector3 GetVelocity() { return velocity; }
+
+
+private:
+
+	Object3d* obj;
+	Model* model;
+	
+	Vector3 position = { 0,0,0 };
+	Vector3 velocity = { 0,0,0 };
+	int r = 1;
+};
+
