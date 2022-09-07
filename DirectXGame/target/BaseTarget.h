ae121dde610ce99f87422ac4f892332f62769d02
newshell <52154054+newshell3125@users.noreply.github.com>
2022-09-07 15:10:01 +0900

    Vector3撲滅

--- DirectXGame/target/BaseTarget.h
+++ DirectXGame/target/BaseTarget.h
@@ -1,6 +1,4 @@
 #pragma once
-#pragma once
-#include "Vector3.h"
 #include "Object3d.h"
 #include "Model.h"
 #include "SafeDelete.h"
@@ -8,24 +6,32 @@
 
 class BaseTarget
 {
+private: // エイリアス
+// Microsoft::WRL::を省略
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+	// DirectX::を省略
+	using XMFLOAT3 = DirectX::XMFLOAT3;
+	using XMVECTOR = DirectX::XMVECTOR;
+
+
 private:
 	BaseTarget();
 	~BaseTarget();
 
 	//初期化
-	virtual void Initialize(std::string modelName, Vector3 pos, int radius);
+	virtual void Initialize(std::string modelName,XMFLOAT3  pos, int radius);
 	//更新
 	virtual void Update();
 	//描画
 	virtual void Draw();
 
 	//Set
-	void SetPosition(Vector3 pos) { position = pos; }
+	void SetPosition(XMFLOAT3 pos) { position = pos; }
 
 	//Get
-	Vector3 GetPosition() { return position; }
-	Vector3 GetVelocity() { return velocity; }
-	int GetR() { return sphere.radius; }
+	XMFLOAT3 GetPosition() { return position; }
+	XMFLOAT3 GetVelocity() { return velocity; }
+	int GetRadius() { return sphere.radius; }
 	Sphere GetSphere() { return sphere; }
 
 private:
@@ -34,7 +40,7 @@ private:
 	Model* model;
 
 	Sphere sphere;
-	Vector3 position = { 0,0,0 };
-	Vector3 velocity = { 0,0,0 };
+	XMFLOAT3 position = { 0,0,0 };
+	XMFLOAT3 velocity = { 0,0,0 };
 };
 
