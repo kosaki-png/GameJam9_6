9d14723593d498b02f56049136ec2a95a2081d8b
skle4b <90739333+SkLE3B@users.noreply.github.com>
2022-09-07 12:06:33 +0900

    Ui枠部分追加
    Ui枠だけ追加

---
+++ DirectXGame/ui/Ui.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "Sprite.h"
+#include "Text.h"
+
+class Ui 
+{
+private: // エイリアス
+// Microsoft::WRL::を省略
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+	// DirectX::を省略
+	using XMFLOAT2 = DirectX::XMFLOAT2;
+	using XMFLOAT3 = DirectX::XMFLOAT3;
+	using XMFLOAT4 = DirectX::XMFLOAT4;
+	using XMVECTOR = DirectX::XMVECTOR;
+	using XMMATRIX = DirectX::XMMATRIX;
+private: // 静的メンバ変数
+	static const int texNumber = 0;
+public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	Ui();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~Ui();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw(ID3D12GraphicsCommandList* cmdList);
+
+private:
+	Sprite* ui;
+	Text* score;
+	Text* timeLimit;
+	float scoreNum;
+};
+
