0d4c5f4177716c8f4a06cdb6a06f4af4cf2b6070
kosaki-png <78684044+kosaki-png@users.noreply.github.com>
2022-09-09 16:36:56 +0900

    スプライトデータ作成

--- DirectXGame/2d/SpriteData.h
+++ DirectXGame/2d/SpriteData.h
@@ -7,7 +7,20 @@ public:
 	static void LoadTexture();
 
 public:
+	// テキスト用
+	const int TEXT_FONT = 0;
 
+	// タイトル用
+	const int TITLE = 1;
+
+	// セレクト用
+	const int SELECT = 11;
+
+	// ゲーム用
+	const int GAME = 21;
+
+	// エンド用
+	const int END = 31;
 
 };
 
